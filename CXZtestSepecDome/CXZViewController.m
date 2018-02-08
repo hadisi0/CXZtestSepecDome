60f4795a6bf4bfa4b93508dc3f86b9d03959ceb7
cheng <cheng@chengdeiMac.local>
2018-02-08 17:34:01 +0800

    first commit

---
+++ CXZtestSepecDome/CXZViewController.m
@@ -0,0 +1,29 @@
+//
+//  CXZViewController.m
+//  CXZtestSepecDome
+//
+//  Created by hadisi0 on 02/08/2018.
+//  Copyright (c) 2018 hadisi0. All rights reserved.
+//
+
+#import "CXZViewController.h"
+
+@interface CXZViewController ()
+
+@end
+
+@implementation CXZViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
